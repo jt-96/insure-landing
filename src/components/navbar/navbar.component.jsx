7a98d60e16a4455f602511bd0279ee3434867f16
jt-96 <jetorrico@outlook.com>
2026-06-03 16:12:49 -0300

    feat: add open/close navbar, add sticky property with fixed links on mobile, extend navbar depending on viewport

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,22 +1,59 @@
+import { useState } from "react";
 import insureLogo from "../../assets/logo.svg";
-import hamburgerClose from "../../assets/icon-close.svg";
+import iconHamburger from "../../assets/icon-hamburger.svg";
+import iconHamburgerClose from "../../assets/icon-close.svg";
 
 function NavBarComponent() {
+  const [isOpen, setIsOpen] = useState(false);
+  const iconSrc = isOpen ? iconHamburgerClose : iconHamburger;
+  const navLinks = [
+    { label: "HOW WE WORK", href: "#" },
+    { label: "BLOG", href: "#" },
+    { label: "ACCOUNT", href: "#" },
+    { label: "VIEW PLANS", href: "#" },
+  ];
+
   return (
-    <>
-      <div>
-        <div className="flex justify-between items-center w-full bg-white p-8">
-          <img src={insureLogo} alt="Insure Logo" />
-          <img src={hamburgerClose} alt="Hamburger Menu Close Icon" />
+    <div className="sticky top-0">
+      <div className="flex bg-white items-center justify-between w-full p-8">
+        <img src={insureLogo} alt="Insure Logo" />
+
+        <div className="hidden items-center gap-8 md:flex">
+          {navLinks.map((link) => (
+            <a
+              key={link.label}
+              className="text-sm font-medium text-slate-700 transition duration-300 hover:text-slate-900"
+              href={link.href}
+            >
+              {link.label}
+            </a>
+          ))}
         </div>
-        <div className="flex flex-col items-center pt-8 pb-70 bg-[url('assets/bg-pattern-mobile-nav.svg')] bg-no-repeat bg-bottom">
-            <a className="w-75 p-4 m-2 border-2 border-transparent transition duration-300 hover:border-white" href="#">HOW WE WORK</a>
-            <a className="w-75 p-4 m-2 border-2 border-transparent transition duration-300 hover:border-white" href="#">BLOG</a>
-            <a className="w-75 p-4 m-2 border-2 border-transparent transition duration-300 hover:border-white" href="#">ACCOUNT</a>
-            <a className="w-75 p-4 m-2 border-2 border-transparent transition duration-300 hover:border-white" href="#">VIEW PLANS</a>
-        </div>
+
+        <button
+          type="button"
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="p-2 md:hidden"
+          aria-label={isOpen ? "Close navigation menu" : "Open navigation menu"}
+        >
+          <img src={iconSrc} alt="Navigation menu icon" />
+        </button>
       </div>
-    </>
+
+      <div
+        className={`${isOpen ? "flex" : "hidden"} fixed w-full flex-col items-center pt-8 pb-70 bg-[url('assets/bg-pattern-mobile-nav.svg')] bg-no-repeat bg-bottom bg-cover`}
+      >
+        {navLinks.map((link) => (
+          <a
+            key={link.label}
+            className="w-75 p-4 m-2 border-2 border-transparent transition duration-300 hover:border-white"
+            href={link.href}
+          >
+            {link.label}
+          </a>
+        ))}
+      </div>
+    </div>
   );
 }
 
