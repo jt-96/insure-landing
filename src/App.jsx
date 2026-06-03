bb4b7e4e468be93c5aea99673b573feb6e78a28e
jt-96 <jetorrico@outlook.com>
2026-06-03 17:47:06 -0300

    feat: add intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBarComponent from "./components/navbar/navbar.component";
+import IntroSection from "./sections/intro/intro.section";
 import "./App.css";
 
 function App() {
   return (
     <>
       <NavBarComponent />
+      <IntroSection />
     </>
   );
 }
