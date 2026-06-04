acf91e7d036fd1214c3b11720caf2c5254a74df9
jt-96 <jetorrico@outlook.com>
2026-06-04 15:06:40 -0300

    feat: add structure and text

--- src/sections/offering/offering.section.jsx
+++ src/sections/offering/offering.section.jsx
@@ -1,5 +1,37 @@
 function OfferingSection() {
+  return (
+    <>
+      <div>
+        <hr />
+        <h2>We're different</h2>
 
+        <div>
+          <span>Logo</span>
+          <h3>Snappy Process</h3>
+          <p>
+            Our application process can be completed in minutes, not hours.
+            Don't get stuck filing in tedious forms.
+          </p>
+        </div>
+        <div>
+          <span>Logo</span>
+          <h3>Affordable Prices</h3>
+          <p>
+            We don't want you worrying about high monthly costs. Our prices may
+            be low, but we still offer the best coverage possible.
+          </p>
+        </div>
+        <div>
+          <span>Logo</span>
+          <h3>People First</h3>
+          <p>
+            Our plans aren't full of conditions and clauses to prevent payouts.
+            We make sure you're covered when you need it.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default OfferingSection;
