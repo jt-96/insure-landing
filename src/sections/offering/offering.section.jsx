334acea706dcca19c7b89f4ef869881cfb7ce28c
jt-96 <jetorrico@outlook.com>
2026-06-04 16:04:14 -0300

    feat: add styling, add logos, add cta

--- src/sections/offering/offering.section.jsx
+++ src/sections/offering/offering.section.jsx
@@ -1,33 +1,61 @@
+import snappyImg from "../../assets/icon-snappy-process.svg";
+import pricesImg from "../../assets/icon-affordable-prices.svg";
+import peopleImg from "../../assets/icon-people-first.svg";
+import workImg from "../../assets/bg-pattern-how-we-work-mobile.svg";
+
 function OfferingSection() {
   return (
     <>
-      <div>
-        <hr />
-        <h2>We're different</h2>
+      <div className="py-40">
+        <div className="flex items-center justify-center">
+          <hr className="w-40 border-1 border-gray-300" />
+        </div>
+        <h2 className="mt-10 mb-20 text-center text-5xl">We're different</h2>
 
-        <div>
-          <span>Logo</span>
-          <h3>Snappy Process</h3>
-          <p>
-            Our application process can be completed in minutes, not hours.
-            Don't get stuck filing in tedious forms.
-          </p>
+        <div className="px-10">
+          <div className="flex flex-col items-center text-center my-10">
+            <img className="mb-5" src={snappyImg} alt="Snappy Process Icon" />
+            <h3 className="my-2 text-2xl">Snappy Process</h3>
+            <p className="my-2 karla-400 px-3">
+              Our application process can be completed in minutes, not hours.
+              Don't get stuck filing in tedious forms.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center my-10">
+            <img
+              className="mb-5"
+              src={pricesImg}
+              alt="Affordable Prices Icon"
+            />
+            <h3 className="my-2 text-2xl">Affordable Prices</h3>
+            <p className="my-2 karla-400 px-3">
+              We don't want you worrying about high monthly costs. Our prices
+              may be low, but we still offer the best coverage possible.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center my-10">
+            <img className="mb-5" src={peopleImg} alt="People First Icon" />
+            <h3 className="my-2 text-2xl">People First</h3>
+            <p className="my-2 karla-400 px-3">
+              Our plans aren't full of conditions and clauses to prevent
+              payouts. We make sure you're covered when you need it.
+            </p>
+          </div>
         </div>
-        <div>
-          <span>Logo</span>
-          <h3>Affordable Prices</h3>
-          <p>
-            We don't want you worrying about high monthly costs. Our prices may
-            be low, but we still offer the best coverage possible.
-          </p>
-        </div>
-        <div>
-          <span>Logo</span>
-          <h3>People First</h3>
-          <p>
-            Our plans aren't full of conditions and clauses to prevent payouts.
-            We make sure you're covered when you need it.
-          </p>
+        <div className="relative flex flex-col items-center bg-[#2c2640] mx-10 py-15">
+          <picture className="absolute right-0 bottom-5">
+            <source media="(max-width: 375px)" srcSet={workImg} />
+            <img src={workImg} />
+          </picture>
+          <h3 className="text-white text-4xl text-center mx-5 mb-5">
+            Find out more about how we work
+          </h3>
+          <a
+            className="karla-700 mt-8 border-2 tracking-widest py-2 px-4 text-white"
+            href="#"
+          >
+            HOW WE WORK
+          </a>
         </div>
       </div>
     </>
