dc1d3d799daf7ecbc42c1f7572cae48d5ce9156a
jt-96 <jetorrico@outlook.com>
2026-06-04 16:26:29 -0300

    feat: add structure

--- src/sections/footer/footer.section.jsx
+++ src/sections/footer/footer.section.jsx
@@ -1,7 +1,48 @@
 function FooterSection() {
     return (
         <>
-        
+            <div>
+                <div>
+                    <p>Insure Logo</p>
+                    <div>
+                        <p>Social Logo</p>
+                        <p>Social Logo</p>
+                        <p>Social Logo</p>
+                        <p>Social Logo</p>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div>
+                    <div>
+                        <h4>OUR COMPANY</h4>
+                        <a href="#">HOW WE WORK</a>
+                        <a href="#">WHY INSURE?</a>
+                        <a href="#">CHECK PRICE</a>
+                        <a href="#">REVIEWS</a>
+                    </div>
+                    <div>
+                        <h4>HELP ME</h4>
+                        <a href="#">FAQ</a>
+                        <a href="#">TERMS OF USE</a>
+                        <a href="#">PRIVACY POLICY</a>
+                        <a href="#">COOKIES</a>
+                    </div>
+                    <div>
+                        <h4>CONTACT</h4>
+                        <a href="#">SALES</a>
+                        <a href="#">SUPPORT</a>
+                        <a href="#">LIVE CHAT</a>
+                    </div>
+                    <div>
+                        <h4>OTHERS</h4>
+                        <a href="#">CAREERS</a>
+                        <a href="#">PRESS</a>
+                        <a href="#">LICENSES</a>
+                    </div>
+                </div>
+            </div>
         </>
     )    
 }
