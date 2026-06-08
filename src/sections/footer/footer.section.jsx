96e53f6d85a869866fe4ad6a2d1cf679c97646e3
jt-96 <jetorrico@outlook.com>
2026-06-08 15:09:47 -0300

    feat: make logos on top of bg, change font & color for sitemap titles

--- src/sections/footer/footer.section.jsx
+++ src/sections/footer/footer.section.jsx
@@ -11,12 +11,12 @@ function FooterSection() {
       <div className="relative flex flex-col items-center bg-[#fafafa]">
         <img className="absolute top-0 left-0 w-full" src={bgFooterMobile} alt="Footer Background" />
         <div className="w-full flex flex-col items-center mt-25">
-          <img className="mb-10" src={insureLogo} alt="Insure Logo" />
+          <img className="mb-10 z-1" src={insureLogo} alt="Insure Logo" />
           <div className="w-40 flex justify-around">
-            <img src={iconFacebook} alt="Facebook Icon" />
-            <img src={iconTwitter} alt="Twitter Icon" />
-            <img src={iconPinterest} alt="Pinterest Icon" />
-            <img src={iconInstagram} alt="Instagram Icon" />
+            <img className="z-1" src={iconFacebook} alt="Facebook Icon" />
+            <img className="z-1" src={iconTwitter} alt="Twitter Icon" />
+            <img className="z-1" src={iconPinterest} alt="Pinterest Icon" />
+            <img className="z-1" src={iconInstagram} alt="Instagram Icon" />
           </div>
         </div>
 
@@ -26,27 +26,27 @@ function FooterSection() {
 
         <div className="flex flex-col mb-10">
           <div className="flex flex-col items-center mb-10">
-            <p className="my-5 karla-400">OUR COMPANY</p>
+            <p className="my-5 karla-700 text-gray-400">OUR COMPANY</p>
             <a href="#" className="my-2 karla-700">HOW WE WORK</a>
             <a href="#" className="my-2 karla-700">WHY INSURE?</a>
             <a href="#" className="my-2 karla-700">CHECK PRICE</a>
             <a href="#" className="my-2 karla-700">REVIEWS</a>
           </div>
           <div className="flex flex-col items-center mb-10">
-            <p className="my-5 karla-400">HELP ME</p>
+            <p className="my-5 karla-700 text-gray-400">HELP ME</p>
             <a href="#" className="my-2 karla-700">FAQ</a>
             <a href="#" className="my-2 karla-700">TERMS OF USE</a>
             <a href="#" className="my-2 karla-700">PRIVACY POLICY</a>
             <a href="#" className="my-2 karla-700">COOKIES</a>
           </div>
           <div className="flex flex-col items-center mb-10">
-            <p className="my-5 karla-400">CONTACT</p>
+            <p className="my-5 karla-700 text-gray-400">CONTACT</p>
             <a href="#" className="my-2 karla-700">SALES</a>
             <a href="#" className="my-2 karla-700">SUPPORT</a>
             <a href="#" className="my-2 karla-700">LIVE CHAT</a>
           </div>
           <div className="flex flex-col items-center mb-10">
-            <p className="my-5 karla-400">OTHERS</p>
+            <p className="my-5 karla-700 text-gray-400">OTHERS</p>
             <a href="#" className="my-2 karla-700">CAREERS</a>
             <a href="#" className="my-2 karla-700">PRESS</a>
             <a href="#" className="my-2 karla-700">LICENSES</a>
