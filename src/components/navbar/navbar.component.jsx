baaccb62bd230a73903cd3fa76efb2119d309d9a
jt-96 <jetorrico@outlook.com>
2026-06-29 16:49:06 -0300

    feat: add animations for navbar and hamburger menu

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import insureLogo from "../../assets/logo.svg";
 import iconHamburger from "../../assets/icon-hamburger.svg";
 import iconHamburgerClose from "../../assets/icon-close.svg";
+
+gsap.registerPlugin(useGSAP);
 
 function NavBarComponent() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,10 +16,58 @@ function NavBarComponent() {
     { label: "ACCOUNT", href: "#" },
     { label: "VIEW PLANS", href: "#" },
   ];
+  const gsapMobileNavContainer = useRef();
+  const mobileNavBarTimeline = useRef();
+  const { contextSafe } = useGSAP({ scope: gsapMobileNavContainer });
+
+  useGSAP(() => {
+    //NavBar Spawning
+    gsap.from(".gsap-container", {
+      duration: 1,
+      y: -100,
+      ease: "power1.out",
+    });
+
+    //Mobile Hamburger Menu Timeline
+    mobileNavBarTimeline.current = gsap.timeline({
+      paused: true,
+      defaults: { ease: "power4.out" },
+    });
+
+    mobileNavBarTimeline.current
+      .fromTo(
+        ".gsap-mobile-nav-container",
+        { height: 0, paddingTop: 0, paddingBottom: 0, autoAlpha: 0 },
+        {
+          height: "375px",
+          paddingTop: 32,
+          paddingBottom: 32,
+          autoAlpha: 1,
+          duration: 0.6,
+          immediateRender: false,
+        },
+      )
+      .from(
+        ".gsap-mobile-nav-button",
+        { y: -12, opacity: 1, stagger: 0.08, duration: 0.25 },
+        0.1,
+      );
+  }, [gsapMobileNavContainer]);
+
+  const handleClick = contextSafe(() => {
+    const nextStateValue = !isOpen;
+    setIsOpen(nextStateValue);
+
+    if (nextStateValue) {
+      mobileNavBarTimeline.current.play();
+    } else {
+      mobileNavBarTimeline.current.reverse();
+    }
+  });
 
   return (
-    <div className="sticky top-0 z-999">
-      <div className="flex bg-white items-center justify-between w-full px-8 py-2 md:px-10 lg:px-20">
+    <div className="sticky top-0 z-999" ref={gsapMobileNavContainer}>
+      <div className="gsap-container flex bg-white items-center justify-between w-full px-8 py-2 md:px-10 lg:px-20">
         <img src={insureLogo} alt="Insure Logo" />
 
         <div className="hidden items-center md:flex">
@@ -32,7 +84,7 @@ function NavBarComponent() {
 
         <button
           type="button"
-          onClick={() => setIsOpen((prev) => !prev)}
+          onClick={handleClick}
           className="p-2 md:hidden"
           aria-label={isOpen ? "Close navigation menu" : "Open navigation menu"}
         >
@@ -40,13 +92,11 @@ function NavBarComponent() {
         </button>
       </div>
 
-      <div
-        className={`${isOpen ? "flex" : "hidden"} flex-col items-center py-8 bg-[#2c2640] bg-[url('assets/bg-pattern-mobile-nav.svg')] bg-no-repeat bg-bottom bg-contain`}
-      >
+      <div className="h-0 opacity-0 overflow-hidden flex gsap-mobile-nav-container flex-col items-center bg-[#2c2640] bg-[url('assets/bg-pattern-mobile-nav.svg')] bg-no-repeat bg-bottom bg-contain">
         {navLinks.map((link) => (
           <a
             key={link.label}
-            className="p-4 m-2 border-2 border-transparent text-white transition duration-300 text-white hover:border-white"
+            className="gsap-mobile-nav-button p-4 m-2 border-2 border-transparent text-white transition duration-300 text-white hover:border-white"
             href={link.href}
           >
             {link.label}
